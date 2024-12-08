39b96c46dfedf2b4914670098b373c341b88dccb
Vitalii Bekshnev <vitalii.bekshnev@gcore.com>
2024-12-08 18:28:25 +0500

    Add JWT auth, sign up and sign in

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from app.db.database import init_db
+from app.models.models import User
 from app.routers import expenses
-from app.routers.auth import router as auth_router
+from app.routers.auth import router as auth_router, get_current_user
 
 app = FastAPI()
 
@@ -11,3 +14,11 @@ app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 @app.get("/")
 async def root():
     return {"message": "Welcome to Expense Tracker API!"}
+
+
+@app.get("/protected")
+def protected_route(current_user: User = Depends(get_current_user)):
+    return {"message": f"Hello, {current_user.username}!"}
+
+
+init_db()
